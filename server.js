1f27ff507341e4ec43c4a6b6c4d2ae6fc6350b01
Marcelo <mccarmo@gmail.com>
2013-08-29 21:33:14 -0300

    adding node.js + socket.io :)

---
+++ server.js
@@ -0,0 +1,37 @@
+var http = require("http"),
+	express = require("express"),
+    fs = require("fs"),
+    socketio = require("socket.io");
+
+var app = express(),
+    server = http.createServer(app);
+
+app.get('/', function (request, response) {
+    fs.createReadStream('./pages/index.html').pipe(response)
+})
+
+app.get('/glMatrix-0.9.5.min.js', function (request, response) {
+    fs.createReadStream('./pages/js/glMatrix-0.9.5.min.js').pipe(response)
+})
+
+app.get('/webgl-utils.js', function (request, response) {
+    fs.createReadStream('./pages/js/webgl-utils.js').pipe(response)
+})
+
+app.get('/geraCubo.js', function (request, response) {
+    fs.createReadStream('./pages/js/geraCubo.js').pipe(response)
+})
+
+var io = socketio.listen(server);
+
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
+
+io.sockets.on('connection', function (socket) {
+	console.log("socket connected...")
+})
+
+server.listen(process.env.PORT || 8002,"127.0.0.1")
+console.log("Server starded...")
