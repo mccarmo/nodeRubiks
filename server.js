b540bb680a977b01b597c445bb1aa54aa59df310
Marcelo <mccarmo@gmail.com>
2014-05-06 21:24:15 -0300

    refactoring the code

--- server.js
+++ server.js
@@ -11,47 +11,51 @@ app.get('/', function (request, response) {
 })
 
 app.get('/glMatrix-0.9.5.min.js', function (request, response) {
-    fs.createReadStream('./pages/js/glMatrix-0.9.5.min.js').pipe(response)
+    fs.createReadStream('./pages/resources/js/glMatrix-0.9.5.min.js').pipe(response)
 })
 
 app.get('/webgl-utils.js', function (request, response) {
-    fs.createReadStream('./pages/js/webgl-utils.js').pipe(response)
+    fs.createReadStream('./pages/resources/js/webgl-utils.js').pipe(response)
 })
 
 app.get('/geraCubo.js', function (request, response) {
-    fs.createReadStream('./pages/js/geraCubo.js').pipe(response)
-})
-
-app.get('/geraCubo.js', function (request, response) {
-    fs.createReadStream('./pages/js/hammer .js').pipe(response)
+    fs.createReadStream('./pages/resources/js/geraCubo.js').pipe(response)
 })
 
 app.get('/blue.png', function (request, response) {
-    fs.createReadStream('./pages/img/blue.png').pipe(response)
+    fs.createReadStream('./pages/resources/img/blue.png').pipe(response)
 })
 
 app.get('/red.png', function (request, response) {
-    fs.createReadStream('./pages/img/red.png').pipe(response)
+    fs.createReadStream('./pages/resources/img/red.png').pipe(response)
 })
 
 app.get('/green.png', function (request, response) {
-    fs.createReadStream('./pages/img/green.png').pipe(response)
+    fs.createReadStream('./pages/resources/img/green.png').pipe(response)
 })
 
 app.get('/white.png', function (request, response) {
-    fs.createReadStream('./pages/img/white.png').pipe(response)
+    fs.createReadStream('./pages/resources/img/white.png').pipe(response)
 })
 
 app.get('/yellow.png', function (request, response) {
-    fs.createReadStream('./pages/img/yellow.png').pipe(response)
+    fs.createReadStream('./pages/resources/img/yellow.png').pipe(response)
 })
 
 app.get('/magent.png', function (request, response) {
-    fs.createReadStream('./pages/img/magent.png').pipe(response)
+    fs.createReadStream('./pages/resources/img/magent.png').pipe(response)
 })
 
 app.get('/nocolor.png', function (request, response) {
-    fs.createReadStream('./pages/img/nocolor.png').pipe(response)
+    fs.createReadStream('./pages/resources/img/nocolor.png').pipe(response)
+})
+
+app.get('/cubeGame.js', function (request, response) {
+    fs.createReadStream('./pages/resources/js/cubeGame.js').pipe(response)
+})
+
+app.get('/style.css', function (request, response) {
+    fs.createReadStream('./pages/resources/css/style.css').pipe(response)
 })
 
 var io = socketio.listen(server);
