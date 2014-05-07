106dbccd89fe9a1ad0fece8e5e228e8d4ff8b5c7
Marcelo Chaves <mccarmo@gmail.com>
2014-05-07 13:52:35 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,58 +6,8 @@ var http = require("http"),
 var app = express(),
     server = http.createServer(app);
 
-app.get('/', function (request, response) {
-    fs.createReadStream('./pages/index.html').pipe(response)
-})
-
-app.get('/glMatrix-0.9.5.min.js', function (request, response) {
-    fs.createReadStream('./pages/resources/js/glMatrix-0.9.5.min.js').pipe(response)
-})
-
-app.get('/webgl-utils.js', function (request, response) {
-    fs.createReadStream('./pages/resources/js/webgl-utils.js').pipe(response)
-})
-
-app.get('/geraCubo.js', function (request, response) {
-    fs.createReadStream('./pages/resources/js/geraCubo.js').pipe(response)
-})
-
-app.get('/blue.png', function (request, response) {
-    fs.createReadStream('./pages/resources/img/blue.png').pipe(response)
-})
-
-app.get('/red.png', function (request, response) {
-    fs.createReadStream('./pages/resources/img/red.png').pipe(response)
-})
-
-app.get('/green.png', function (request, response) {
-    fs.createReadStream('./pages/resources/img/green.png').pipe(response)
-})
-
-app.get('/white.png', function (request, response) {
-    fs.createReadStream('./pages/resources/img/white.png').pipe(response)
-})
-
-app.get('/yellow.png', function (request, response) {
-    fs.createReadStream('./pages/resources/img/yellow.png').pipe(response)
-})
-
-app.get('/magent.png', function (request, response) {
-    fs.createReadStream('./pages/resources/img/magent.png').pipe(response)
-})
-
-app.get('/nocolor.png', function (request, response) {
-    fs.createReadStream('./pages/resources/img/nocolor.png').pipe(response)
-})
-
-app.get('/cubeGame.js', function (request, response) {
-    fs.createReadStream('./pages/resources/js/cubeGame.js').pipe(response)
-})
-
-app.get('/style.css', function (request, response) {
-    fs.createReadStream('./pages/resources/css/style.css').pipe(response)
-})
-
+app.use(express.static(__dirname + '/pages'));
+	
 var io = socketio.listen(server);
 
 io.configure(function () { 
