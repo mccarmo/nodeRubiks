70c3b6ca586c9475fbc4265972c3b449c4f03dca
Marcelo <mccarmo@gmail.com>
2014-06-05 23:15:43 -0300

    Added cssmin task to Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,25 +7,41 @@ module.exports = function(grunt) {
        js: {
          files: ['pages/resources/js/*.js'],
          tasks: ['uglify']
-       }
+       },
+       css: {
+	 files: ['pages/resources/css/*.css'],
+         tasks: ['cssmin']
+       }	
      },
      uglify: {
        all: {
          files: {
 	   'pages/resources/js/min/main.min.js': [
+	   'pages/resources/js/glMatrix-0.9.5.min.js',
+	   'pages/resources/js/webgl-utils.js',
            'pages/resources/js/geraCubo.js',
 	   'pages/resources/js/cubeGame.js'
            ]
          }
        },
+     },
+     cssmin: {
+       minify: {
+	 expand: true,
+	 cwd: 'pages/resources/css/',
+	 src: ['*.css'],
+	 dest: 'pages/resources/css/min/',
+	 ext: '.min.css'
+       }
      }
    });
 
    // Load the plugins.
    grunt.loadNpmTasks('grunt-contrib-watch');
    grunt.loadNpmTasks('grunt-contrib-uglify');
+   grunt.loadNpmTasks('grunt-contrib-cssmin');
    
    // Default task(s).
-   grunt.registerTask('default', ['uglify' , 'watch']);
+   grunt.registerTask('default', ['uglify' , 'cssmin' , 'watch']);
    
 };
